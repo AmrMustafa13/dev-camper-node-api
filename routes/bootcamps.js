4e98a03ea825aba47280c9dd392fb339112f0640
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-02-08 23:58:19 +0200

    filter and sort results added

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,9 +7,13 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 } = require("../controllers/bootcamps");
 
 // Create a route for the controller
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
 router.get("/", getBootcamps).post("/", createBootcamp);
 
 router
